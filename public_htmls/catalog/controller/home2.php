828af7f08f1d5cf6d55e5966f3ec610847ee5f54
Tikkub <tikkubzaza@gmail.com>
2024-10-10 19:23:08 +0700

    fghfdd

---
+++ public_htmls/catalog/controller/home2.php
@@ -0,0 +1,14 @@
+<?php 
+	class Home2Controller extends Controller {
+		public function index() {
+	    	$data = array(); 
+			$data['tables'] = $this->model('master')->getTable();
+			$this->view('home',$data); 
+	    }
+		public function getTable(){
+			$tables = array();
+			$tables = $this->model('master')->getTable()->rows;
+			$this->json($tables); 
+		}
+	}
+?>
